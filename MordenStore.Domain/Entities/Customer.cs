944acf1674c2118eac8f1953d3acea789c5c6615
LzrCampos <32502509+LzrCampos@users.noreply.github.com>
2017-10-21 21:06:55 -0200

    adicionando notificações

--- MordenStore.Domain/Entities/Customer.cs
+++ MordenStore.Domain/Entities/Customer.cs
@@ -1,10 +1,11 @@
-﻿using MordenStore.Domain.ValueObject;
+﻿using ModernStore.Share.Entities;
+using MordenStore.Domain.ValueObject;
 using System;
 using System.Collections.Generic;
 
 namespace MordenStore.Domain.Entities
 {
-    public class Customer
+    public class Customer : Entity
     {
         public Customer(User user, Name name, Email email, DateTime? birthDate)
         {
@@ -14,20 +15,20 @@ namespace MordenStore.Domain.Entities
             this.BirthDate = null;
 
             // Validações
-            if (Name.FirstName.Length < 3)
-                Notifications.Add("FirstName", "Nome invalido");
-            if (Name.LastName.Length < 3)
-                Notifications.Add("LastName", "Nome invalido");
-            if (Email.email.Length < 3)
-                Notifications.Add("Email", "Nome invalido");
+            //if (Name.FirstName.Length < 3)
+            //    Notifications.Add("FirstName", "Nome invalido");
+            //if (Name.LastName.Length < 3)
+            //    Notifications.Add("LastName", "Nome invalido");
+            //if (Email.email.Length < 3)
+            //    Notifications.Add("Email", "Nome invalido");
+
+
         }
 
         public User User { get; private set; }
         public Name Name { get; private set; }
         public Email Email { get; private set; }
         public DateTime? BirthDate { get; set; }
-
-        public Dictionary<string, string> Notifications { get; private set; }
 
         public override string ToString()
         {
