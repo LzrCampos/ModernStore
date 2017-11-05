5eb767035119878d9a6f73ad2552f66cad00de14
LzrCampos <32502509+LzrCampos@users.noreply.github.com>
2017-11-04 22:23:59 -0200

    Usando o EntityFramework.

--- ModernStore.Infrastructure/Contexts/ModernStoreDataContext.cs
+++ ModernStore.Infrastructure/Contexts/ModernStoreDataContext.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿using MordenStore.Domain.Entities;
+using System;
+using System.Data.Entity;
 
 namespace ModernStore.Infrastructure.Contexts
 {
-    public class ModernStoreDataContext
+    public class ModernStoreDataContext : DbContext
     {
+        public ModernStoreDataContext() : base(@"Server=.\SQLEXPRESS; Database=ModernStore; User Id=lazaro; password= 5289")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
 
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ ModernStore.Infrastructure/Mappings/CustomerMap.cs
@@ -0,0 +1,28 @@
+﻿using MordenStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernStore.Infrastructure.Mappings
+{
+    public class CustomerMap : EntityTypeConfiguration<Customer>
+    {
+        public CustomerMap()
+        {
+            ToTable("Customer");
+            HasKey(x => x.Id);
+            Property(x => x.BirthDate);
+            Property(x => x.Document.DocumentNumber);
+            Property(x => x.Email.EmailAdress);
+            Property(x => x.Name.FirstName);
+            Property(x => x.Name.LastName);
+            Property(x => x.User.UserName);
+            Property(x => x.User.Password);
+            Property(x => x.User.Active);
+
+        }
+    }
+}
